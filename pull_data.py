bbe9bbf837b4b37b65f869fcafe44755366c3dc6
Jonathan Pichot <jonathan@pichot.us>
2016-10-13 16:10:31 -0400

    reproducable data import

---
+++ pull_data.py
@@ -0,0 +1,109 @@
+import os
+import glob
+import requests
+import zipfile
+
+project_dir = os.path.dirname(__file__)
+dl_path = os.path.join(project_dir, "data/external")
+
+# Create data directories if needed
+for d in ["data", "data/external", "data/interim", "data/processed"]:
+    if not os.path.exists(d):
+        os.makedirs(d)
+
+# Citibike stations
+citibike_file = dl_path + "/citibike-stations.json"
+if not os.path.isfile(citibike_file):
+    print("Downloading Citibike stations")
+    r = requests.get('https://feeds.citibikenyc.com/stations/stations.json',
+                     stream=True)
+
+    with open(citibike_file, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024):
+            if chunk:
+                f.write(chunk)
+
+# Tree Canopy
+trees_file = dl_path + "/tree-canopy.geojson"
+if not os.path.isfile(trees_file):
+    print("Downloading Tree Canopy")
+    r = requests.get('https://data.cityofnewyork.us/api/geospatial/pi5s-9p35',
+                     params={"method": "export", "format": "GeoJSON"},
+                     stream=True)
+
+    with open(trees_file, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024):
+            if chunk:
+                f.write(chunk)
+
+# Stree Assessment
+streets_file = dl_path + "/street-assessment.geojson"
+if not os.path.isfile(streets_file):
+    print("Downloading Street Assessment")
+    r = requests.get('http://www.nyc.gov/html/dot/downloads/misc/street-assessment-rating.geojson',
+                     stream=True)
+
+    with open(streets_file, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024):
+            if chunk:
+                f.write(chunk)
+
+# Subway Entrances
+subway_file = dl_path + "/subway-entrances.geojson"
+if not os.path.isfile(subway_file):
+    print("Downloading Subway Entrances")
+    r = requests.get('https://data.cityofnewyork.us/api/geospatial/drex-xx56',
+                     params={"method": "export", "format": "GeoJSON"},
+                     stream=True)
+
+    with open(subway_file, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024):
+            if chunk:
+                f.write(chunk)
+
+# Bike Routes
+bike_zip = dl_path + "/bike-lanes.zip"
+if not os.path.isfile(bike_zip):
+    print("Downloading Bike Lanes")
+    r = requests.get('http://www.nyc.gov/html/dot/downloads/misc/nyc-bike-routes.zip',
+                     stream=True)
+
+    with open(bike_zip, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024):
+            if chunk:
+                f.write(chunk)
+
+    zip = zipfile.ZipFile(bike_zip)
+    zip.extractall(dl_path + '/bike-lanes')
+
+# Traffic Short Counts
+traffic_short_zip = dl_path + "/traffic-short-counts.zip"
+if not os.path.isfile(traffic_short_zip):
+    print("Downloading Traffic Short Counts")
+    r = requests.get('https://www.dot.ny.gov/divisions/engineering/applications/traffic-data-viewer/traffic-data-viewer-repository/TDV_Shapefile_Short_Counts_2014.zip',
+                     params={"accessType": "DOWNLOAD"},
+                     stream=True)
+
+    with open(traffic_short_zip, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024):
+            if chunk:
+                f.write(chunk)
+
+    zip = zipfile.ZipFile(traffic_short_zip)
+    zip.extractall(dl_path + '/traffic-short-counts')
+
+# Traffic Continuous Counts
+traffic_continuous_zip = dl_path + "/traffic-continuous-counts.zip"
+if not os.path.isfile(traffic_continuous_zip):
+    print("Downloading Traffic Continuous Counts")
+    r = requests.get('https://www.dot.ny.gov/divisions/engineering/applications/traffic-data-viewer/traffic-data-viewer-repository/TDV_Shapefile_Continuous_Counts_2014.zip',
+                     params={"accessType": "DOWNLOAD"},
+                     stream=True)
+
+    with open(traffic_continuous_zip, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024):
+            if chunk:
+                f.write(chunk)
+
+    zip = zipfile.ZipFile(traffic_continuous_zip)
+    zip.extractall(dl_path + '/traffic-continuous-counts')
--- src/data/0.1-kz-citibike-stations.py
+++ src/data/0.1-kz-citibike-stations.py
@@ -1,63 +1,21 @@
-
-#Get Citibike station location data
-
-#url format:  https://feeds.citibikenyc.com/stations/stations.json
-
-
 import json
-import urllib
-import codecs
-import pandas as pd
-import numpy as np
 import os
 
-os.chdir("..")
-os.chdir("..")
-if not os.path.exists("data/external"):
-    os.makedirs("data/external")
+output_csv = os.getcwd() + '/data/processed/stations.csv'
 
-os.chdir("data/external")
+with open(os.getcwd() + '/data/external/citibike-stations.json') as data_file:
+    data_js = json.load(data_file)
 
-if not os.path.isfile('citibike_station_info.csv'):
-    citibike_serviceurl = "https://feeds.citibikenyc.com/stations/stations.json"
+if not os.path.isfile(output_csv):
+    fh_cb = open(output_csv, 'w+')
+    fh_cb.writelines('Station_id,Station_Name,Location,Latitude,Longitude')
 
-    while True:
+    all_station_info = data_js['stationBeanList']
+    for station in all_station_info:
+        cb_station_id = station['id']
+        cb_station_name = station['stationName']
+        cb_location = station['stAddress1']
+        lat = station['latitude']
+        lon = station['longitude']
 
-        url = citibike_serviceurl
-        print 'Retrieving', url
-
-        data = urllib.urlopen(url).read()
-        try:
-            data_js = json.loads(str(data))
-        except:
-            data_js = None
-            print '==Failure to Retrieve=='
-            continue
-
-    #fhand = codecs.open('citibike_loc_json.js', 'w', "utf-8")   #create a json file to inspect the format and content
-    #fhand.writelines(json.dumps(data_js, indent=4))
-
-        fname = 'citibike_station_info.csv'
-        fh_cb = open(fname, 'w+')
-        fh_cb.writelines('Station_id,Station_Name,Location,Latitude,Longitude')
-
-        all_station_info = data_js['stationBeanList']
-        for station in all_station_info:
-            cb_station_id = station['id']
-            cb_station_name = station['stationName']
-            cb_location = station['stAddress1']
-            lat = station['latitude']
-            lon = station['longitude']
-
-        #print '{},{},{},{},{},{},{},{},{},{},{},{}'.format(cb_station_id, cb_station_name,cb_location,lat,lon,tot_docks,avb_docks,avb_bikes,last_commu_time,status,is_test_station,status_key)
-            fh_cb.writelines('\n{},{},{},{},{}'.format(cb_station_id, cb_station_name,cb_location,lat,lon))
-
-            station_count = len(all_station_info)
-    #print '\nNumber of Citi Bike stations ', station_count
-
-        break
-
-#create DataFrame using Pandas
-#fh_cb = open(fname, 'r')
-#df_cb = pd.read_csv(fname)
-#df_cb.head(20)
+        fh_cb.writelines('\n{},{},{},{},{}'.format(cb_station_id, cb_station_name,cb_location,lat,lon))
